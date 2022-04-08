412754dff0f07c2c7f95c549e18d0433a8c8c1db
Alex Bustos <alexfbp@gmail.com>
2022-04-08 11:29:24 -0500

    feat: auditoria de utils_oas + `go mod tidy`

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/plugins/cors"
-	"github.com/udistrital/auditoria"
 	_ "github.com/udistrital/terceros_mid/routers"
 	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
+	"github.com/udistrital/utils_oas/auditoria"
 	"github.com/udistrital/utils_oas/customerrorv2"
 )
 
