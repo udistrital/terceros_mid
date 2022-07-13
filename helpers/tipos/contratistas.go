76a6366b5f03bb3578afa2eb57c8a19fcc4ece38
Alex F. Bustos P <alexfbp@gmail.com>
2022-07-13 10:06:30 -0500

    refactor: consulta de contratistas

--- helpers/tipos/contratistas.go
+++ helpers/tipos/contratistas.go
@@ -1,41 +1,32 @@
 package tipos
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
-	"strconv"
+	"strings"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
-	"github.com/mitchellh/mapstructure"
 
+	TercerosCrudModels "github.com/udistrital/terceros_crud/models"
+	TercerosHelper "github.com/udistrital/terceros_mid/helpers/crud/terceros"
 	"github.com/udistrital/terceros_mid/models"
-	e "github.com/udistrital/utils_oas/errorctrl"
 	"github.com/udistrital/utils_oas/request"
 )
 
 // GetContratista trae la lista de contratistas registrados en Terceros, con opción de filtrar por ID
 func GetContratista(idTercero int, query string) (terceros []map[string]interface{}, outputError map[string]interface{}) {
 	const funcion = "GetContratista - "
-
 	step := "0"
-
 	defer func() {
 		if err := recover(); err != nil {
 			outputError = map[string]interface{}{
-				"funcion": "GetContratista - Uncaught Error! - after step:" + step,
+				"funcion": funcion + "uncaught error after step:" + step,
 				"err":     err,
 				"status":  "500", // Uncaught error!
 			}
 			panic(outputError)
 		}
 	}()
-
-	if query != "" {
-		err := errors.New("query no implementado")
-		return nil, e.Error(funcion+`query != ""`, err, fmt.Sprint(http.StatusNotImplemented))
-	}
 
 	// PARTE 1. Traer los ID de los parámetros asociados a contratistas
 
@@ -97,134 +88,64 @@ func GetContratista(idTercero int, query string) (terceros []map[string]interfac
 	// logs.Debug("parametroContratistaID:", parametroContratistaID)
 
 	// PARTE 2. Traer los terceros que tengan los ID anteriores en la tabla vinculacion
-	tercerosMap := make(map[int](map[string]interface{}))
-	for k, paramID := range parametroContratistaID {
-		step = "2.1_" + k
-		var vinculaciones []models.Vinculacion
-		urlTerceros := "http://" + beego.AppConfig.String("tercerosService") + "vinculacion?limit=-1"
-		urlTerceros += "&fields=Id,TerceroPrincipalId"
-		urlTerceros += "&query=Activo:true,TipoVinculacionId:" + fmt.Sprint(paramID)
-		if idTercero > 0 {
-			urlTerceros += ",TerceroPrincipalId__Id:" + fmt.Sprint(idTercero)
-		}
-		// logs.Debug("urlTerceros:", urlTerceros)
-		if resp, err := request.GetJsonTest(urlTerceros, &vinculaciones); err == nil && resp.StatusCode == 200 {
-			step = "2.2_" + k
-			if len(vinculaciones) == 0 || vinculaciones[0].TerceroPrincipalId == nil {
-				continue
-			}
-			// fmt.Println("paramId:", paramId, "#vinculaciones: ", len(vinculaciones))
+	empty := []string{}
 
-			// Lo siguiente es para que no se vuelva a agregar un tercero
-			// cuando el tercero tenga más de una vinculación
-			step = "2.3_" + k
-			for k2, vincul := range vinculaciones {
-				if _, found := tercerosMap[vincul.TerceroPrincipalId.Id]; found {
-					continue
-				}
-				step = "2.4.1_" + k + "_" + strconv.Itoa(k2)
-				terceroRecortado := map[string]interface{}{
-					"Id":             vincul.TerceroPrincipalId.Id,
-					"NombreCompleto": vincul.TerceroPrincipalId.NombreCompleto,
-					// "UsuarioWSO2":    vincul.TerceroPrincipalId.UsuarioWSO2,
-				}
-				tercerosMap[vincul.TerceroPrincipalId.Id] = map[string]interface{}{
-					// "Tercero": vincul.TerceroPrincipalId,
-					"Tercero": terceroRecortado,
-					// "TipoVinculacion":  vincul.TipoVinculacionId,
-				}
-				step = "2.4.2_" + k + "_" + strconv.Itoa(k2)
-			}
-			step = "2.5_" + k
-
-		} else {
-			if err == nil {
-				err = fmt.Errorf("Undesired status code - Got:%d", resp.StatusCode)
-			}
-			logs.Error(err)
-			outputError = map[string]interface{}{
-				"funcion": "GetContratista - request.GetJsonTest(urlTerceros, &vinculaciones)",
-				"err":     err,
-				"status":  "502",
-			}
-			return nil, outputError
-		}
-
+	// vinculacionesMap := make(map[int]models.Vinculacion)
+	var vinculos = []string{}
+	for _, id := range parametroContratistaID {
+		vinculos = append(vinculos, fmt.Sprint(id))
 	}
-	step = "2.6"
-	for _, tercero := range tercerosMap {
-		terceros = append(terceros, tercero)
+	var vinculacionesTerceros []TercerosCrudModels.Vinculacion
+	fullQueryVinculaciones := "Activo:true"
+	if idTercero > 0 {
+		fullQueryVinculaciones += ",TerceroPrincipalId:" + fmt.Sprint(idTercero)
 	}
-	step = "2.7"
-	// logs.Debug("terceros:", terceros)
-
-	// PARTE 3 Traer identificación disponible...
-	for k, tercero := range terceros {
-		kStr := strconv.Itoa(k)
-
-		step = "3.1_" + kStr
-		var terceroModelo models.Tercero
-		if err := mapstructure.Decode(tercero["Tercero"], &terceroModelo); err != nil {
-			logs.Error(err)
-			outputError = map[string]interface{}{
-				"funcion": "GetContratista - mapstructure.Decode(tercero[\"Tercero\"], &terceroModelo)",
-				"err":     err,
-				"status":  "500",
-			}
-			return nil, outputError
-		}
-		// logs.Debug("terceroModelo:", terceroModelo)
-		step = "3.2_" + kStr
-		// 3.1 ... de terceros?
-		var dataTerceros []map[string]interface{} // models.DatosIdentificacion
-		urlDocTercero := "http://" + beego.AppConfig.String("tercerosService") + "datos_identificacion"
-		urlDocTercero += "?fields=TipoDocumentoId,Numero"
-		urlDocTercero += "&query=Activo:true,TerceroId__Id:" + fmt.Sprint(terceroModelo.Id)
-		// logs.Debug("urlDocTercero: ", urlDocTercero)
-		if resp, err := request.GetJsonTest(urlDocTercero, &dataTerceros); err == nil && resp.StatusCode == 200 {
-			step = "3.3_" + kStr
-			// tercero["DataTercerosDocumento"] = dataTerceros[0]
-			if len(dataTerceros) == 1 && dataTerceros[0]["Id"] != 0 {
-				step = "3.4_" + kStr
-				var dataModel models.DatosIdentificacion
-				if err := mapstructure.Decode(dataTerceros[0], &dataModel); err != nil {
-					logs.Error(err)
-					outputError = map[string]interface{}{
-						"funcion": "GetContratista - mapstructure.Decode(dataTerceros[0], &dataModel)",
-						"err":     err,
-						"status":  "500",
-					}
-					return nil, outputError
-				}
-				step = "3.5_" + kStr
-				dataRecortada := map[string]interface{}{
-					// "TipoDocumentoId": dataModel.TipoDocumentoId,
-					"TipoDocumentoId": map[string]interface{}{
-						"Id":     dataModel.TipoDocumentoId.Id,
-						"Nombre": dataModel.TipoDocumentoId.Nombre,
-					},
-					"Numero": dataModel.Numero,
-				}
-				tercero["Identificacion"] = dataRecortada
-				step = "3.6_" + kStr
-			} else {
-				err := fmt.Errorf("Hay +/- un documento registrado como Activo para el Tercero con ID: %d", terceroModelo.Id)
-				logs.Warn(err)
-			}
-		} else {
-			if err == nil {
-				err = fmt.Errorf("Undesired status code - Got:%d", resp.StatusCode)
-			}
-			logs.Error(err)
-			outputError = map[string]interface{}{
-				"funcion": "GetContratista - request.GetJsonTest(urlDocTercero, &dataTerceros)",
-				"err":     err,
-				"status":  "502",
-			}
-			return nil, outputError
-		}
-
+	fullQueryVinculaciones += "TipoVinculacionId__in:" + strings.Join(vinculos, "|")
+	limit := -1
+	offset := 0
+	fieldsVinculaciones := []string{"Id", "TerceroPrincipalId"}
+	if err := TercerosHelper.GetVinculaciones(&vinculacionesTerceros, fullQueryVinculaciones, limit, offset, fieldsVinculaciones, empty, empty); err != nil {
+		outputError = err
+		return
 	}
 
-	return terceros, nil
+	tercerosMap := make(map[int]TercerosCrudModels.Tercero)
+	for _, v := range vinculacionesTerceros {
+		tercerosMap[v.TerceroPrincipalId.Id] = *v.TerceroPrincipalId
+	}
+
+	documentosMap := make(map[int]TercerosCrudModels.DatosIdentificacion)
+	for terceroId := range tercerosMap {
+		fullQueryDocumentos := "Activo:true,TerceroId__Activo:true,TerceroId__Id:" + fmt.Sprint(terceroId)
+		fieldsDocumentos := []string{"Id", "TipoDocumentoId", "Numero"}
+		var documentosTerceros []TercerosCrudModels.DatosIdentificacion
+		if err := TercerosHelper.GetDatosIdentificacion(&documentosTerceros, fullQueryDocumentos, limit, offset, fieldsDocumentos, empty, empty); err != nil {
+			outputError = err
+			return
+		}
+		for k, v := range documentosTerceros {
+			documentosMap[k] = v
+			*documentosMap[k].TerceroId = tercerosMap[terceroId]
+		}
+	}
+
+	for _, v := range documentosMap {
+		terceros = append(terceros, map[string]interface{}{
+			"Tercero": map[string]interface{}{
+				"Id":             v.TerceroId.Id,
+				"NombreCompleto": v.TerceroId.NombreCompleto,
+				// "UsuarioWSO2":    vincul.TerceroPrincipalId.UsuarioWSO2,
+			},
+			"Identificacion": map[string]interface{}{
+				// "TipoDocumentoId": dataModel.TipoDocumentoId,
+				"TipoDocumentoId": map[string]interface{}{
+					"Id":     v.TipoDocumentoId.Id,
+					"Nombre": v.TipoDocumentoId.Nombre,
+				},
+				"Numero": v.Numero,
+			},
+		})
+	}
+
+	return
 }
